f78c91012ca73773ed35ee8c7d661a5bb6ee9f31
@dmitrii_sadokhin_1 <dim23333@gmail.com>
2024-02-23 11:02:31 +0300

    ddd

--- src/main/java/com/example/rest/demo/exception/UpdateStateException.java
+++
@@ -1,8 +0,0 @@
-package com.example.rest.demo.exception;
-
-public class UpdateStateException extends RuntimeException{
-    public UpdateStateException(String message) {
-        super(message);
-    }
-}
-
