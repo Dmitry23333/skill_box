5cc1f04c8b29773c043191a57af3422d1b833bcc
Dmitry23333 <68802269+Dmitry23333@users.noreply.github.com>
2024-08-08 14:49:24 +0300

    add

---
+++ spring-jwt-auth-example/src/main/java/com/example/spring_jwt_auth_example/web/model/RefreshTokenResponse.java
@@ -0,0 +1,14 @@
+package com.example.spring_jwt_auth_example.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenResponse {
+    private String accessToken;
+    private String refreshToken;
+}
